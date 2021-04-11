5eb607453a04123295ccdeb2d26c25c27d6d2d06
Badrivishal Paurana <badrivishalpaurana@gmail.com>
2021-04-11 18:23:44 +0530

    Version 2.3

--- Project2.py
+++ Project2.py
@@ -4,23 +4,7 @@ from harris_detector import *
 import imutils
 import os
 
-# address1 = 'imagesSet2/2.jpg'
-# # address1 = 'imagesSet1/uttower_left.jpg'
-# # address1 = 'image1.jpg'
-# image1 = cv.imread(address1)
-# image1 = cv.cvtColor(image1, cv.COLOR_BGR2RGB)
-# gray1 = cv.cvtColor(image1, cv.COLOR_RGB2GRAY)
-# # plt.imshow(image1)
-
-# address2 = 'imagesSet2/3.jpg'
-# # address2 = 'imagesSet1/uttower_right.jpg'
-# # address2 = 'image2.jpg'
-# image2 = cv.imread(address2)
-# image2 = cv.cvtColor(image2, cv.COLOR_BGR2RGB)
-# gray2 = cv.cvtColor(image2, cv.COLOR_RGB2GRAY)
-# plt.imshow(image2)
-
-def stitching(addresses, threshold1, threshold2):
+def stitching(addresses, threshold1, threshold2, detectorType = 'brisk'):
 
     address1, address2 = addresses
     image1 = cv.imread(address1)
@@ -31,74 +15,66 @@ def stitching(addresses, threshold1, threshold2):
     image2 = cv.cvtColor(image2, cv.COLOR_BGR2RGB)
     gray2 = cv.cvtColor(image2, cv.COLOR_RGB2GRAY)
 
-    # harris_in1 = gray1[-image2.shape[0]:, :]
-    im, lst1 = harris_corner_detection(gray1, threshold1)
-    im, lst2 = harris_corner_detection(gray2, threshold2)
-
-    # sift = cv.BRISK_create()
-    sift = cv.ORB_create()
+    kp1 = harris_corner_detection(gray1, threshold1)
+    kp2 = harris_corner_detection(gray2, threshold2)
 
 
-    kp1, desc1 = sift.compute(gray1, lst1)
-    kp2, desc2 = sift.compute(gray2, lst2)
+    if detectorType == 'sift':
+        featureDetector = cv2.xfeatures2d.SIFT_create()
+        bMatcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+    elif detectorType == 'surf':
+        featureDetector = cv2.xfeatures2d.SURF_create()
+        bMatcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+    elif detectorType == 'brisk':
+        featureDetector = cv2.BRISK_create()
+        bMatcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    elif detectorType == 'orb':
+        featureDetector = cv2.ORB_create()
+        bMatcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
 
-    bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    matches2 = bf.match(desc1,desc2)
-    matches2 = sorted(matches2, key = lambda x:x.distance)
 
-    print(len(matches2), len(desc1), len(desc2))
+    kp1, desc1 = featureDetector.compute(gray1, kp1)
+    kp2, desc2 = featureDetector.compute(gray2, kp2)
 
-    img3 = cv.drawMatches(image1, kp1, image2, kp2, matches2[:10], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    matches = bMatcher.match(desc1,desc2)
+    matches = sorted(matches, key = lambda x:x.distance)
 
-    plt.imshow(img3)
+    print(len(matches), len(desc1), len(desc2))
+
+    matchesIm = cv.drawMatches(image1, kp1, image2, kp2, matches[:10], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    plt.imshow(matchesIm)
     plt.show()
 
-    image_1_points = np.zeros((len(matches2), 1, 2), dtype=np.float32)
-    image_2_points = np.zeros((len(matches2), 1, 2), dtype=np.float32)
+    pts1 = np.zeros((len(matches), 1, 2), dtype=np.float32)
+    pts2 = np.zeros((len(matches), 1, 2), dtype=np.float32)
 
-    for i in range(0,len(matches2)):
-        image_1_points[i] = kp1[matches2[i].queryIdx].pt
-        image_2_points[i] = kp2[matches2[i].trainIdx].pt
+    for i in range(0,len(matches)):
+        pts1[i] = kp1[matches[i].queryIdx].pt
+        pts2[i] = kp2[matches[i].trainIdx].pt
 
-    (H, status) = cv.findHomography(image_2_points, image_1_points, cv.RANSAC, 4)
+    (H, status) = cv.findHomography(pts2, pts1, cv.RANSAC, 4)
     print(H)
-    print(len(matches2), len(kp1))
+    print(len(matches), len(kp1))
 
     width = image2.shape[1] + image1.shape[1]
     height = image2.shape[0] + image1.shape[0]
 
     result = cv.warpPerspective(image2, H, (width, height))
-
-    # result[0:image1.shape[0], 0:image1.shape[1]] = image1
-    # mask = np.where(result==0, 1, 0)
-    # result[0:image1.shape[0], 0:image1.shape[1]] = cv2.bitwise_and(image1, result[0:image1.shape[0], 0:image1.shape[1]], mask)
     
     for i in range(image1.shape[0]):
         for j in range(image1.shape[1]):
             if result[i,j].all() == 0:
                 result[i,j] = image1[i,j]
 
-    # plt.imshow(result)
-    # plt.axis('off')
-    # plt.show()
 
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
-
-    # Finds contours from the binary image
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-
-    # get the maximum contour area
-    c = max(cnts, key=cv2.contourArea)
-
-    # get a bbox from the contour area
-    (x, y, w, h) = cv2.boundingRect(c)
-
-    # crop the image to the bbox coordinates
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    maxContour = max(contours, key=cv2.contourArea)
+    (x, y, w, h) = cv2.boundingRect(maxContour)
     result = result[y:y + h, x:x + w]
-
-    # show the cropped image
     plt.imshow(result)
     plt.show()
 
@@ -120,4 +96,4 @@ def panorama(folder):
         threshold1 = threshold1*0.2
     return None
 
-panorama('imagesSet3')
+panorama('imagesSet1')
